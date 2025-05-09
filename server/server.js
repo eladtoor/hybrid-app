52724944970e561b424bda06a6887217f16cc363
eladtoor <eladtoordev@gmail.com>
2025-05-09 12:22:08 +0300

    Fixing CSP

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
       "script-src 'self' 'unsafe-inline' https://apis.google.com https://cdn.gtranslate.net https://translate.google.com https://translate.googleapis.com https://translate-pa.googleapis.com; " +
       "style-src 'self' 'unsafe-inline' https://www.gstatic.com; " +
       "font-src 'self' https: data:; " +
-      "connect-src 'self' https://hybrid-app-1-2haj.onrender.com https://lavangroup.co.il https://firestore.googleapis.com wss://hybrid-app-1-2haj.onrender.com https://identitytoolkit.googleapis.com https://translate.googleapis.com; " +
+      "connect-src 'self' https://hybrid-app-1-2haj.onrender.com https://lavangroup.co.il https://firestore.googleapis.com wss://hybrid-app-1-2haj.onrender.com https://identitytoolkit.googleapis.com https://translate.googleapis.com https://www.lavangroup.co.il; " +
       "frame-src 'self' https://*.firebaseapp.com; " +
       "frame-ancestors 'none'; " +
       "object-src 'none'; " +
