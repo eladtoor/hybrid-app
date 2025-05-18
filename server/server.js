6f1f77db23d28e4f6e945c54f1e2ffc196bf29fd
eladtoor <eladtoordev@gmail.com>
2025-05-18 17:39:07 +0300

    google api

--- server/server.js
+++ server/server.js
@@ -66,7 +66,7 @@ app.use((req, res, next) => {
     "default-src 'self'; " +
       "img-src 'self' https: data:; " +
       "script-src 'self' 'unsafe-inline' https://apis.google.com https://cdn.gtranslate.net https://translate.google.com https://translate.googleapis.com https://translate-pa.googleapis.com https://vee-crm.com; " +
-      "script-src-elem 'self' 'unsafe-inline' https://vee-crm.com https://cdn.gtranslate.net https://translate.google.com; " +
+      "script-src-elem 'self' 'unsafe-inline' https://vee-crm.com https://cdn.gtranslate.net https://translate.google.com https://translate.googleapis.com; " +
       "style-src 'self' 'unsafe-inline' https://vee-crm.com https://www.gstatic.com; " +
       "font-src 'self' https: data:; " +
       "connect-src 'self' https://vee-crm.com https://cdn.gtranslate.net https://translate.googleapis.com https://translate-pa.googleapis.com https://hybrid-app-1-2haj.onrender.com https://lavangroup.co.il https://firestore.googleapis.com wss://hybrid-app-1-2haj.onrender.com https://identitytoolkit.googleapis.com https://www.lavangroup.co.il https://securetoken.googleapis.com https://api.cloudinary.com; " +
